9e45efa5add6b378c164d4ca224b8b6b66b05377
rs0579 <raheemsenegal@outlook.com>
2025-04-02 00:40:40 +0200

    made alteration to code so that it can deploy to render.

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -12,3 +12,24 @@ db.once('open', async () => {
   console.log('Questions seeded!');
   process.exit(0);
 });
+
+
+// import db from '../config/connection.js';
+// import { Question } from '../models/index.js'
+// import cleanDB from './cleanDb.js';
+
+// import questionData from './pythonQuestions.json' with { type: 'json'};
+
+// try {
+//   await db();
+//   await cleanDB();
+
+//   // bulk create each model
+//   await Question.insertMany(questionData);
+
+//   console.log('Seeding completed successfully!');
+//   process.exit(0);
+// } catch (error) {
+//   console.error('Error seeding database:', error);
+//   process.exit(1);
+// }
