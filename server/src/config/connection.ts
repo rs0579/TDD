5ac29f434ad9732ab3d3f2f2c563e21f4d62b848
rs0579 <raheemsenegal@outlook.com>
2025-04-02 01:56:34 +0200

    Reversed changes for render deployment

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || '3001');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 
 export default mongoose.connection;
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import routes from './routes/index.js';
 
 
 const app = express();
-const PORT = process.env.PORT || 3001 || 'mongodb://127.0.0.1:27017/techquiz';
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
