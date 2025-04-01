63990069dac9ebd3a4e449eac8e9d59bb75f70a8
rs0579 <raheemsenegal@outlook.com>
2025-04-02 01:26:15 +0200

    change the port in the connection.ts and server.ts for render deployment

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,6 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose.connect(process.env.MONGODB_URI || '3001');
 
 export default mongoose.connection;
